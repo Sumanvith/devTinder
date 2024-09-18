8646cd0dab8c1741d9fc86e500f33b365bcee986
Sumanvith <dellbilla@gmail.com>
2024-09-18 17:16:11 +0530

    EP4

--- src/app.js
+++ src/app.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
 
+app.get("/user/:userid", (req, res) => {
+  console.log(req.params);
+  res.send({ firstName: "Sumanvith", lastName: "GP" });
+});
+
+app.post("/user", (req, res) => {
+  res.send("Data sent");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Deleted");
+});
+
 app.use("/test", (req, res) => {
   res.send("Hello");
 });
