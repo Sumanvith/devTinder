9440cd29721aa3d1eaa7920946d071167ebaf18a
Sumanvith <dellbilla@gmail.com>
2024-09-18 20:18:08 +0530

    EP4 1/3

--- src/app.js
+++ src/app.js
@@ -1,22 +1,33 @@
 const express = require("express");
 const app = express();
 
-app.get("/user/:userid", (req, res) => {
-  console.log(req.params);
-  res.send({ firstName: "Sumanvith", lastName: "GP" });
-});
+app.get(
+  "/user",
+  (req, res, next) => {
+    console.log("Handling the route user!!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 2!!");
+    // res.send("2nd Response!!");
+    next();
+  },
 
-app.post("/user", (req, res) => {
-  res.send("Data sent");
-});
-
-app.delete("/user", (req, res) => {
-  res.send("Deleted");
-});
-
-app.use("/test", (req, res) => {
-  res.send("Hello");
-});
+  (req, res, next) => {
+    console.log("Handling the route user 3!!");
+    // res.send("3rd Response!!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 4!!");
+    // res.send("4th Response!!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 5!!");
+    res.send("5th Response!!");
+  }
+);
 
 app.listen(3000, () => {
   console.log("running");
