e42e212fd634a5e421d3dc4c3c444b1b7660695f
Sumanvith <dellbilla@gmail.com>
2024-09-19 21:53:51 +0530

    EP8

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ const { connectDB } = require("./config/database");
 const express = require("express");
 const User = require("./models/user");
 const app = express();
-
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
@@ -34,17 +33,29 @@ app.delete("/user", async (req, res) => {
   }
 });
 
-app.patch("/user", async (req, res) => {
-  const userId = req.body.userId;
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
   const data = req.body;
+
   try {
+    const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills"];
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATES.includes(k)
+    );
+    if (!isUpdateAllowed) {
+      throw new Error("Update not allowed");
+    }
+    if (data?.skills.length > 10) {
+      throw new Error("Skills cannot be more than 10");
+    }
     const user = await User.findByIdAndUpdate({ _id: userId }, data, {
       returnDocument: "after",
+      runValidators: true,
     });
     console.log(user);
     res.send("User updated successfully");
   } catch (err) {
-    res.status(400).send("Something went wrong ");
+    res.status(400).send("UPDATE FAILED:" + err.message);
   }
 });
 
