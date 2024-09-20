5035b4a33c6433ec062f756d29d06dab83e862b8
Sumanvith <dellbilla@gmail.com>
2024-09-20 21:20:53 +0530

    EP13

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const authRouter = express.Router();
+
 const { validateSignUpData } = require("../utils/validation");
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
+
 authRouter.post("/signup", async (req, res) => {
   try {
     validateSignUpData(req);
@@ -15,22 +17,27 @@ authRouter.post("/signup", async (req, res) => {
       emailId,
       password: passwordHash,
     });
+
     await user.save();
     res.send("User Added successfully!");
   } catch (err) {
     res.status(400).send("ERROR : " + err.message);
   }
 });
+
 authRouter.post("/login", async (req, res) => {
   try {
     const { emailId, password } = req.body;
+
     const user = await User.findOne({ emailId: emailId });
     if (!user) {
       throw new Error("Invalid credentials");
     }
     const isPasswordValid = await user.validatePassword(password);
+
     if (isPasswordValid) {
       const token = await user.getJWT();
+
       res.cookie("token", token, {
         expires: new Date(Date.now() + 8 * 3600000),
       });
@@ -49,4 +56,5 @@ authRouter.post("/logout", async (req, res) => {
   });
   res.send("Logout Successful!!");
 });
+
 module.exports = authRouter;
